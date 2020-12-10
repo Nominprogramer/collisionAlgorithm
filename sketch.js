af6ecbe9c26b51c1ca49fd469e847764c48c19cf
Nominprogramer <58177990+Nominprogramer@users.noreply.github.com>
2020-12-10 20:45:06 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,30 +1,47 @@
-var fixedRect, movingRect;
+let fixedRect,movingRect,o1,o2,o3,o4;
 
 function setup() {
-  createCanvas(1200,800);
-  fixedRect = createSprite(600, 400, 50, 80);
-  fixedRect.shapeColor = "green";
-  fixedRect.debug = true;
-  movingRect = createSprite(400,200,80,30);
-  movingRect.shapeColor = "green";
-  movingRect.debug = true;
+  createCanvas(800,400);
+  fixedRect = createSprite(400, 200, 50, 50);
+  movingRect=createSprite(50,100,60,90);
+  o1=createSprite(50,200,60,90);
+  o2=createSprite(100,200,60,90);
+  o3=createSprite(150,200,60,90);
+  o4=createSprite(200,200,60,90);
 }
 
 function draw() {
-  background(0,0,0);  
-  movingRect.x = World.mouseX;
-  movingRect.y = World.mouseY;
+  background(255,255,255); 
+  movingRect.x=World.mouseX
+  movingRect.y=World.mouseY
+   
+  if(istouching(movingRect,fixedRect)){
+    movingRect.shapeColor="red";
+    fixedRect.shapeColor="red";
+  }
+  else{
+    movingRect.shapeColor="green";
+    fixedRect.shapeColor="green";
 
-  if (movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2
-      && fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2
-      && movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2
-      && fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2) {
-    movingRect.shapeColor = "red";
-    fixedRect.shapeColor = "red";
   }
-  else {
-    movingRect.shapeColor = "green";
-    fixedRect.shapeColor = "green";
-  }
+
   drawSprites();
+}
+
+function istouching(mr,sr){
+
+  if(mr.x-sr.x <mr.width/2+sr.width/2  
+    && sr.x-mr.x<mr.width/2+sr.width/2 
+    && mr.y-sr.y <mr.height/2+sr.height/2 
+     && sr.y-mr.y<mr.height/2+sr.height/2)
+  {
+
+ return true;
+  }
+  
+  
+else {
+
+  return false;
+}
 }
